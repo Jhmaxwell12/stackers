32cb3d064416b399f48e0f968faceb4d8e8ebc4c
Jhmaxwell12 <jhmaxwell12@gmail.com>
2018-07-25 11:13:07 -0500

    pixel moving across bottom row

---
+++ stackers.py
@@ -0,0 +1,28 @@
+from sense_hat import SenseHat
+from pygame.locals import *
+import pygame
+import time
+
+#class structure
+sense = SenseHat()
+sense.clear()
+
+class stack():
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_mode((640, 480))
+        self.gaming = True
+    
+    def startGame(self):
+        pygame.time.set_timer(USEREVENT +1, 800)
+        while self.gaming:
+            for i in range(0,7):
+                sense.set_pixel(i, 7, (0, 0, 255))
+                time.sleep(1)
+                sense.clear()
+if __name__ == "__main__":
+    try:
+        game = stack()
+        game.startGame()
+    except KeyboardInterrupt:
+        sense.clear()
